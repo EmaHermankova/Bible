54a4db52be68c8325c5bdc80a26d22690575fa7d
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-04-04 09:17:26 +0200

    Add files via upload

--- main.java
+++ main.java
@@ -2,24 +2,33 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
-public class main{
+public class main {
     public static void main(String[] args) throws IOException {
+        ArrayList hledam = new ArrayList();
+
         File nya = new File("Z:\\complet.txt");
         FileReader pur = new FileReader(nya, StandardCharsets.UTF_8);
 
-        int x = 0;
-        try (FileReader fr = new FileReader(nya)) {
-            int content;
-            while ((content = fr.read()) != -1) {
-                char ch = (char) content;
-                if (ch == 'a' || ch == 'A') {
-                    x++;
+        for (; ;) {
+            int j = pur.read();
+            if (j == -1)
+                break;
+
+            char ch = (char) j;
+            ch = Character.toLowerCase(ch);
+            boolean jeNovy = true;
+            for (int k = 0; k < hledam.size(); k++) {
+                if (ch == (char) hledam.get(k)) {
+                    jeNovy = false;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            if (jeNovy) {
+                System.out.print(" "+ch);
+                hledam.add(ch);
+            }
         }
-        System.out.println(x);
     }
 }
